934b4e3eab0b1f02057fd3fe7d2f1a2039e2dba2
Hamed Mousavi <me@hamedmousavi.com>
2023-10-30 13:25:45 +0000

    adding config file

---
+++ config.php
@@ -0,0 +1,22 @@
+<!-- connection to database -->
+<?php
+
+//start the session
+session_start();
+
+//connect to database
+$db_server = "localhost";
+$db_username = "root";
+$db_password = "root";
+$db_name = "auction"; 
+
+//create connection to database
+$conn = mysqli_connect($db_server, $db_username, $db_password, $db_name);
+$conn->set_charset("utf8");
+
+//check connection
+if (mysqli_connect_errno()) {
+    echo 'Failed to connect to the MySQLserver: '. mysqli_connect_error();
+}
+
+?>
