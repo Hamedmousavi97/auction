ee0a3f5eb2ab615037d326d36694a7379f243bd7
Hamed Mousavi <me@hamedmousavi.com>
2023-11-02 18:03:30 +0000

    database connection fixed

--- process_registration.php
+++ process_registration.php
@@ -1,7 +1,64 @@
+<?php require_once("config.php")?>
+<?php include_once("browse.php")?>
+
 <?php
 
 // TODO: Extract $_POST variables, check they're OK, and attempt to create
 // an account. Notify user of success/failure and redirect/give navigation 
 // options.
 
+
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $email = $_POST['email'];
+  $query = "INSERT INTO users (userName, userPassword, userEmail) VALUES ('$username', '$password', '$email')";
+  if (!mysqli_query($conn, $query)) {
+    //die('Error: ' . mysqli_error($connection));
+    echo 'Failed to connect to the MySQLserver: '. mysqli_connect_error();
+  } else {
+    header("Location: browse.php");
+    mysqli_close($conn);
+
+    exit();
+  }
+
+    // // Assuming you have already established a database connection and stored it in the $connection variable.
+
+    // // Check if the HTTP request method is POST (assumed in your form).
+    // if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    //     // Sanitize and validate user inputs (not shown in this code, but it's important).
+    //     $username = $_POST['username'];
+    //     $password = password_hash($_POST['password'], PASSWORD_BCRYPT); // Hash the password.
+    //     $email = $_POST['email'];
+
+    //     // Create a prepared statement.
+    //     $query = "INSERT INTO users (userName, userPassword, userEmail) VALUES (?, ?, ?)";
+    //     $stmt = mysqli_prepare($connection, $query);
+
+    //     if ($stmt) {
+    //         // Bind parameters and execute the query.
+    //         mysqli_stmt_bind_param($stmt, "sss", $username, $password, $email);
+    //         if (mysqli_stmt_execute($stmt)) {
+    //             // Query executed successfully.
+    //             header("Location: header.php");
+    //             exit();
+    //         } else {
+    //             // Error occurred during query execution.
+    //             die('Error: ' . mysqli_stmt_error($stmt));
+    //         }
+            
+    //         // Close the prepared statement.
+    //         mysqli_stmt_close($stmt);
+    //     } else {
+    //         // Error in preparing the statement.
+    //         die('Error: ' . mysqli_error($connection));
+    //     }
+    // }
+
+    // // Close the database connection when done.
+    // mysqli_close($connection);
+
+
 ?>
+
+
--- register.php
+++ register.php
@@ -1,5 +1,4 @@
 <?php include_once("header.php")?>
-<?php include_once("config.php")?>
 
 <div class="container">
 <h2 class="my-3">Register new account</h2>
@@ -23,21 +22,21 @@
   <div class="form-group row">
     <label for="email" class="col-sm-2 col-form-label text-right">Email</label>
 	<div class="col-sm-10">
-      <input type="text" class="form-control" id="email" placeholder="Email">
+      <input type="text" class="form-control" id="email" placeholder="Email" name="email">
       <small id="emailHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
 	</div>
   </div>
   <div class="form-group row">
     <label for="username" class="col-sm-2 col-form-label text-right">User Name</label>
 	<div class="col-sm-10">
-      <input type="text" class="form-control" id="username" placeholder="User Name">
+      <input type="text" class="form-control" id="username" placeholder="User Name" name="username">
       <small id="username" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
 	</div>
   </div>
   <div class="form-group row">
     <label for="password" class="col-sm-2 col-form-label text-right">Password</label>
     <div class="col-sm-10">
-      <input type="password" class="form-control" id="password" placeholder="Password">
+      <input type="password" class="form-control" id="password" placeholder="Password" name="password">
       <small id="passwordHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
     </div>
   </div>
@@ -56,35 +55,5 @@
 <div class="text-center">Already have an account? <a href="" data-toggle="modal" data-target="#loginModal">Login</a>
 
 </div>
-
-<?php
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Check if the HTTP request method is POST, which means the form was submitted.
-
-    // Retrieve user data from the form
-    $username = $_POST['username'];
-    $password = password_hash($_POST['password'], PASSWORD_BCRYPT); // Hash the password
-    $email = $_POST['email'];
-
-    // Insert user data into the 'users' table
-    $sql = "INSERT INTO users (userName, userPassword, userEmail) VALUES (?, ?, ?)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("sss", $username, $password, $email);
-
-    if ($stmt->execute()) {
-        // Registration was successful
-        echo "User registered successfully.";
-    } else {
-        // An error occurred during registration
-        echo "Error registering user: " . $stmt->error;
-    }
-
-    $stmt->close();
-}
-
-$conn->close();
-?>
-
 
 <?php include_once("footer.php")?>
