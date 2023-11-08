c2fb70b5e374afddb13a7230f9643e5c834a092b
Sunny <sunnyfungcy@gmail.com>
2023-11-08 18:19:43 +0000

    Updated registration
    registration part can work, the only problem is "Invalid email. Please enter a valid email address" is not showing when i put the wrong email format. Fortunately it's not adding to the database as well.

--- process_registration.php
+++ process_registration.php
@@ -12,6 +12,27 @@ include_once("browse.php");
   $password = $_POST['password'];
   $email = $_POST['email'];
   $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+
+// email registration - checking the email format 
+function sanitizeEmail($email) {
+  // remove the space in front or behind the email
+  $email = trim($email);
+
+  // remove illegal characters
+  $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+  // verify email format
+  if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    return $email;
+  } else {
+    return false; // verify fails, return false
+  }
+}
+
+$sanitizedEmail = sanitizeEmail($email);
+
+if ($sanitizedEmail) {
+  // Verify success, do something
   $query = "INSERT INTO users (UserName, UserPassword, UserEmail) VALUES ('$username', '$hashedPassword', '$email')";
   if (!$conn->multi_query($query)) {
     echo 'Failed to connect to the MySQLserver: '. mysqli_connect_error();
@@ -19,8 +40,10 @@ include_once("browse.php");
     header("Location: browse.php");
     mysqli_close($conn);
 
-    //exit();
   }
-
+} else {
+  // Verify fails, do something
+  echo "Invalid email. Please enter a valid email address.";
+}
 
 ?>
