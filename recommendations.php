b5cbfa8a73affc279b3c719dd67e0dc4e4798954
Sunny <sunnyfungcy@gmail.com>
2023-11-26 16:27:56 +0000

    Recommendations halfway finished
    Waiting for the bid function then I'll finalise it.

--- recommendations.php
+++ recommendations.php
@@ -2,10 +2,9 @@
 <?php require("utilities.php")?>
 
 <div class="container">
+  <h2 class="my-3">Recommendations for you</h2>
 
-<h2 class="my-3">Recommendations for you</h2>
-
-<?php
+  <?php
   // This page is for showing a buyer recommended items based on their bid 
   // history. It will be pretty similar to browse.php, except there is no 
   // search bar. This can be started after browse.php is working with a database.
@@ -14,9 +13,36 @@
   
   
   // TODO: Check user's credentials (cookie/session).
-  
+  if (!isset($_SESSION['userId'])) {
+    // Redirect to login page, or show an error message
+    echo "<p>Please log in to view recommendations.</p>";
+    exit;
+  }
+
+  $userId = $_SESSION['userId'];
+
+  // Database connection setup
+  require_once("config.php");
+  $conn = mysqli_connect($db_server, $db_username, $db_password, $db_name);
+  if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+  }
+
   // TODO: Perform a query to pull up auctions they might be interested in.
-  
+  $query = "SELECT * FROM auctions WHERE auctionID IN (SELECT auctionID FROM bids WHERE userID = '$userId') ORDER BY auctionEndDate DESC";
+  $result = mysqli_query($conn, $query);
+
   // TODO: Loop through results and print them out as list items.
-  
+  if ($result && mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+      print_listing_li($row['auctionID'], $row['auctionTitle'], $row['auctionDetails'], $row['auctionStartPrice'], $row['numBids'], $row['auctionEndDate'], $row['UserName'], $row['auctionCategory'], $row['auctionReservePrice']);
+    }
+  } else {
+    echo "<p>No recommendations available based on your bid history.</p>";
+  }
+
+  mysqli_close($conn);
 ?>
+</div>
+
+<?php include_once("footer.php")?>
